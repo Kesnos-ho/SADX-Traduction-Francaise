b13254c7e987f13ccaf3ad9c26b3fae6814ff8fb
Kesnos-ho <76844828+Kesnos-ho@users.noreply.github.com>
2021-12-03 19:33:56 +0100

    Correction de bugs 2
    Les caractères spéciaux devraient maintenant s'afficher correctement.

--- sadx-traduction-francaise/Mod.cpp
+++ sadx-traduction-francaise/Mod.cpp
@@ -28,8 +28,8 @@ extern "C"
 	__declspec(dllexport) __declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
 	{
 		// Chao Garden Hint Monitor
-		WriteData((const char**)0x887738, "QUITTER");
-		WriteData((const char**)0x887734, "SUIVANT");
+		WriteData((const char**)0x887738, "QUIT.");
+		WriteData((const char**)0x887734, "SUIV.");
 		WriteData((const char**)0x887730, "RETOUR");
 		
 		// Unlock Chars
@@ -38,66 +38,66 @@ extern "C"
 		WriteData((const char**)0x9169D4, "\tTu peux maintenant jouer avec AMY");
 		WriteData((const char**)0x9169E8, "\tTu peux maintenant jouer avec E-102");
 		WriteData((const char**)0x9169FC, "\tTu peux maintenant jouer avec BIG");
-		WriteData((const char**)0x4B5433, "\tTu peux maintenant jouer avec METAL SONIC \ndans la sélection de niveau");
+		WriteData((const char**)0x4B5433, "\tTu peux maintenant jouer avec METAL SONIC \ndans la s\u00e9lection de niveau");
 		
 		// Game menu messages
-		WriteData((const char**)0x917DCC, "Restaurer les commandes par défaut ?"); // fr
-		WriteData((const char**)0x917DE4, "Appuie sur ESC pour retourner en arrière"); // fr
+		WriteData((const char**)0x917DCC, "Restaurer les commandes par d\u00e9faut ?"); // fr
+		WriteData((const char**)0x917DE4, "Appuie sur \u00e9chap. pour retourner en arri\u00e8re"); // fr
 		WriteData((const char**)0x917DF8, "Utiliser cette touche ?"); // fr
-		WriteData((const char**)0x917E0C, "Appuie sur la touche à utiliser"); // fr
-		WriteData((const char**)0x90C4A8, "Cette action mettra fin à la partie et renverra le\npersonnage à l'écran de sélection, continuer ?"); // fr
-		WriteData((const char**)0x90C4BC, "Cette action mettra fin au niveau et\nte renverra à l'écran de sélection, continuer ?"); // fr
-		WriteData((const char**)0x90C4D0, "Quitter la partie et retourner au Menu\nprincipal. continuer ?"); // fr
-		WriteData((const char**)0x4B54DB, "'%s'\na été ajouté à la liste de jeux Game Gear."); // fr
+		WriteData((const char**)0x917E0C, "Appuie sur la touche \u00e0 utiliser"); // fr
+		WriteData((const char**)0x90C4A8, "En es-tu s\u00fbr ? Cette action mettra fin \u00e0 la partie et \nte \u00e0 l'\u00e9cran de s\u00e9lection."); // fr
+		WriteData((const char**)0x90C4BC, "En es-tu s\u00fbr ? Cette action mettra fin au niveau et \nte renverra dans la zone d'exploration."); // fr
+		WriteData((const char**)0x90C4D0, "Es-tu s\u00fbr de vouloir quitter la partie ? \nCela te renverra au menu principal."); // fr
+		WriteData((const char**)0x4B54DB, "'%s'\na \u00e9t\u00e9 ajout\u00e9 \u00e0 la liste de jeux Game Gear."); // fr
 		
 		// New lure messages
-		WriteData((const char**)0x9818A0, "\aPowerup de l'appât!"); // fr
+		WriteData((const char**)0x9818A0, "\aPowerup de l'app\u00e2t!"); // fr
 		WriteData((const char**)0x9818A4, "\aUtilise le pour attraper de\nplus gros poissons !"); // fr
 		
 		// Keys messages
 		WriteData((const char**)0x111C6A8, "\aLa clef flotte..."); // fr
-		WriteData((const char**)0x111C680, "\aCela doit être une autre clef..."); // fr
-		WriteData((const char**)0x7D3AB1, "\aUne statue en argent...\nElle ressemble à une clef."); // fr
-		WriteData((const char**)0x7D3A72, "\aUne statue en or...\nElle ressemble à une clef."); // fr
+		WriteData((const char**)0x111C680, "\aCela doit \u00eatre une autre clef..."); // fr
+		WriteData((const char**)0x7D3AB1, "\aUne statue en argent...\nElle ressemble \u00e0 une clef."); // fr
+		WriteData((const char**)0x7D3A72, "\aUne statue en or...\nElle ressemble \u00e0 une clef."); // fr
 		WriteData((const char**)0x7D3A2B, "\aLa Wind Stone. On dirait une clef.\nLe motif sur la pierre doit contenir un message."); // fr
-		WriteData((const char**)0x7D39E0, "\aL'Ice Stone. On dirait une clef.\nJe pourrais peut être l'utiliser aux Mystic Ruins."); // fr
-		WriteData((const char**)0x2BBF538, "\aL'Ice Stone... On dirait une clef.\nJe pourrais peut être l'utiliser aux Mystic Ruins."); // fr
+		WriteData((const char**)0x7D39E0, "\aL'Ice Stone. On dirait une clef.\nJe pourrais peut \u00eatre l'utiliser aux Mystic Ruins."); // fr
+		WriteData((const char**)0x2BBF538, "\aL'Ice Stone... On dirait une clef.\nJe pourrais peut \u00eatre l'utiliser aux Mystic Ruins."); // fr
 		
 		// Deleting file messages	
 		WriteData((const char**)0x10D7A44, "\aValider"); // fr
 		WriteData((const char**)0x10D7A0C, "\aAnnuler");
 		WriteData((const char**)0x10D7A6C, "\aEffacer");
 		
-		WriteData((const char**)0x10D7954, "\t\tSupprimé."); // ак
+		WriteData((const char**)0x10D7954, "\t\tSupprim\u00e9."); // ак
 
-		WriteData((const char**)0x10D7968, "\t\tLa suppression a échoué."); // fr
+		WriteData((const char**)0x10D7968, "\t\tLa suppression a \u00e9chou\u00e9."); // fr
 		
 		// Monkey Detonator messages
 
 		WriteData((const char**)0x111CB68, "\aÇa dit \"destructeur de singes\"."); // fr
 
 
-		WriteData((const char**)0x111CB6C, "\a'Appuyez sur le bouton pour détruire le singe."); // fr
-		WriteData((const char**)0x111CB38, "\aInterrupteur activé!"); // fr
-		WriteData((const char**)0x111CB10, "\aLe bouton a été pressé,\nmais rien ne s'est passé."); // fr
+		WriteData((const char**)0x111CB6C, "\a'Appuyez sur le bouton pour d\u00e9truire le singe."); // fr
+		WriteData((const char**)0x111CB38, "\aInterrupteur activ\u00e9 !"); // fr
+		WriteData((const char**)0x111CB10, "\aLe bouton a \u00e9t\u00e9 press\u00e9,\nmais rien ne s'est pass\u00e9."); // fr
 
 		// Station Square cards messages
-		WriteData((const char**)0x2BC07B8, "\aUne carte d'employé! Je peux l'utiliser\npour entrer dans ce bâtiment."); // fr
-		WriteData((const char**)0x2BC07E0, "\aVérification de l'identité"); // fr
+		WriteData((const char**)0x2BC07B8, "\aLa carte d'un employ\u00e9 ! Je pourrais peut-être \nl'utiliser pour entrer dans ce b\u00e2timent."); // fr
+		WriteData((const char**)0x2BC07E0, "\aV\u00e9rification de l'identit\u00e9"); // fr
 		WriteData((const char**)0x2BBF470, "\aUne carte-passe ! Je vais pouvoir faire\nle Twinkle Circuit."); // fr
-		WriteData((const char**)0x2BBF468, "\aBienvenue à Twinkle Circuit!"); // fr
+		WriteData((const char**)0x2BBF468, "\aBienvenue \u00e0 Twinkle Circuit!"); // fr
 		
 		// Tikal's messages
 		WriteData((const char**)0x88C730, "\aPar ici!"); //  fr
-		WriteData((const char**)0x803484, "\aAttention! Il prépare un mauvais coup."); // fr
+		WriteData((const char**)0x803484, "\aAttention! Il pr\u00e9pare un mauvais coup."); // fr
 		// Chaos 0  (when timer is 01:00:00)
 		WriteData((const char**)0x7FD51C, "\aSautez et appuyez de nouveau sur le\nbouton de saut pour faire une Homing Attack!");
 		// Chaos 2
-		WriteData((const char**)0x7FD960, "\aVisez la tête de Chaos\nquand il ne s'y attend pas!");
+		WriteData((const char**)0x7FD960, "\aVisez la t\u00eate de Chaos\nquand il ne s'y attend pas!");
 		WriteData((const char**)0x7FD9B0, "\aPlanez ou utilisez la roulade\nau lieu de le frapper pour le battre!");
 		WriteData((const char**)0x7FDA00, "\aFrappez les\nbulles d'eau!");
 		// Chaos 4
-		WriteData((const char**)0x7FE450, "\aQuand Chaos sort la tête de l'eau,\nattaquez-le.");
+		WriteData((const char**)0x7FE450, "\aQuand Chaos sort la t\u00eate de l'eau,\nc'est le moment de l'attaquer.");
 		// Chaos 6
 		WriteData((const char**)0x7FEBA8, "\aEssayez de trouver un moyen\nde geler Chaos.");
 		WriteData((const char**)0x7FEBF8, "Jetez les bombes d'Eggman\ndans la bouche de Chaos.");
@@ -111,12 +111,12 @@ extern "C"
 		WriteData((const char**)0x802D10, "\aMais ne visez pas que les jambes.\nVisez aussi le cockpit!");
 
 		// Egg Viper
-		WriteData((const char**)0x803444, "\aEggman est de l'autre côté\nlà où l'énergie se rassemble.");
+		WriteData((const char**)0x803444, "\aEggman est de l'autre côt\u00e9\nlà où l'\u00e9nergie se rassemble.");
 		WriteData((const char**)0x803454, "\aFaites plusieurs Homing Attack...");
 		WriteData((const char**)0x803464, "\aEt visez le cockpit.");
 		
 		// if you take damage twice from a spinning thing
-		WriteData((const char**)0x803474, "\aMontez sur la toupie à épines\net vous arriverez jusqu'au cockpit.");
+		WriteData((const char**)0x803474, "\aMontez sur la toupie à \u00e9pines\net vous arriverez jusqu'au cockpit.");
 		
 		// ZERO (when timer is 01:00:00)
 		WriteData((const char**)0x8046E4, "\aIl doit bien avoir un point faible.");
@@ -124,24 +124,24 @@ extern "C"
 		
 		// Perfect Chaos
 		WriteData((const char**)0x800544, "\aQuand vous atteindrez votre vitesse maximale,\nvous deviendrez une boule électrique.");
-		WriteData((const char**)0x80054C, "\aC'est avec ça que vous parviendrez à attaquer Chaos!");
+		WriteData((const char**)0x80054C, "\aC'est avec \u00e7a que vous parviendrez \u00e0 attaquer Chaos!");
 
 		//Mission tutorial texts
 		//region fr
 		WriteData((const char**)0x2BC57B8, "Ton but est de trouver ");
 		WriteData((const char**)0x2BC57BC, "\"l'enveloppe de mission\"");
-		WriteData((const char**)0x2BC57C0, "puis de la compléter");
+		WriteData((const char**)0x2BC57C0, "puis de la compl\u00e9ter");
 		WriteData((const char**)0x2BC57C4, "dans le monde du jeu");
-		WriteData((const char**)0x2BC57C8, "grâce au conseil affiché.");
+		WriteData((const char**)0x2BC57C8, "gr\u00e2ce au conseil affich\u00e9.");
 		
 		WriteData((const char**)0x2BC57CC, "Le conseil s'affichera en");
 		WriteData((const char**)0x2BC57D0, "touchant \"l'enveloppe de");
 		WriteData((const char**)0x2BC57D4, "mission\". Tu devras alors deviner");
-		WriteData((const char**)0x2BC57D8, "l'endroit où il faut aller et");
-		WriteData((const char**)0x2BC57DC, "ce qu'il faut faire pour réussir cette mission.");
+		WriteData((const char**)0x2BC57D8, "l'endroit o\u00f9 il faut aller et");
+		WriteData((const char**)0x2BC57DC, "ce qu'il faut faire pour r\u00e9ussir cette mission.");
 
 		WriteData((const char**)0x2BC57E0, "Il existe 60 missions et");
-		WriteData((const char**)0x2BC57E4, "6 personnages pour les compléter (Sonic, Tails,");
+		WriteData((const char**)0x2BC57E4, "6 personnages pour les compl\u00e9ter (Sonic, Tails,");
 		WriteData((const char**)0x2BC57E8, "Knuckles, Amy, E-102,");
 		WriteData((const char**)0x2BC57EC, "Big).");
 
